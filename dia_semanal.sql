c4799550a1b1314cb7e6efbcd50292b359b0d97e
Uriel4443 <182829@upslp.edu.mx>
2025-11-05 16:44:48 -0600

    Update dia_semanal.sql

--- dia_semanal.sql
+++ dia_semanal.sql
@@ -1,1 +1,24 @@
+CREATE TABLE dias_semana (
+  id_dia SERIAL PRIMARY KEY,
+  nombre TEXT
+  );
+INSERT INTO dias_semana (nombre) VALUES
+('Lunes'),
+('Martes'),
+('Miercoles'),
+('Jueves'),
+('Viernes'),
+('Sabado'),
+('Domingo');
 
+CREATE OR REPLACE FUNCTION dia_semanal(dia_elejido DATE)
+RETURNS TEXT AS $$
+DECLARE
+    nombre_dia TEXT;
+BEGIN
+    SELECT d.nombre INTO nombre_dia FROM dias_semana d WHERE d.id_dia = EXTRACT(ISODOW FROM dia_elejido);
+    RETURN nombre_dia;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT dia_semanal('2025-11-05');
